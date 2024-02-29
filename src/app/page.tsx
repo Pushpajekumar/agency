9209bd39202a8ae89f20c1cc097fd2e11a935811
Pushpajekumar <mk6358522@gmail.com>
2024-02-29 21:52:42 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "./__components/hero-section";
 import Services from "./__components/services";
 import Portfolio from "./__components/portfolio";
+import { NavbarDemo } from "./__components/navbar";
 
 export default function Home() {
   return (
     <main>
+      <NavbarDemo />
       <HeroSection />
       <Services />
       <Portfolio />
