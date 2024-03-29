0e5f932f0f40f879e3d8785e4d9c2e0267695ca3
Pushpajekumar <mk6358522@gmail.com>
2024-03-29 20:05:54 +0530

    fixing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <CardHoverEffectDemo />
       <Portfolio />
       <Testimonials />
-      <BackgroundBeamsDemo />
+      {/* <BackgroundBeamsDemo /> */}
       <Faqs />
     </main>
   );
